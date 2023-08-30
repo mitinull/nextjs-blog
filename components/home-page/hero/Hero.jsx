f1d1feceaff2ed5bd33f91ff03b09cb8ff0eb0fa
mohammadT9030 <torabi9030@gmail.com>
2023-08-30 17:16:15 +0330

    changing hero text

--- components/home-page/hero/Hero.jsx
+++ components/home-page/hero/Hero.jsx
@@ -19,7 +19,7 @@ export const Hero = () => {
       </div>
       <h2 className={styles.heading}>Hi, I'm Mohammad</h2>
       <p className={styles.paragraph + " " + font1.className}>
-        I made this experimental blog to learn Next.js features!
+        I made this blog to learn and test Next.js features!
       </p>
     </section>
   );
