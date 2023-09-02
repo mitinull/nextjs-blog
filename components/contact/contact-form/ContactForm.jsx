16c573d89cb60b8d2b3d28ee203eae04c6f9c77d
mohammadT9030 <torabi9030@gmail.com>
2023-09-02 12:30:02 +0330

    Adding handle submit function and required

--- components/contact/contact-form/ContactForm.jsx
+++ components/contact/contact-form/ContactForm.jsx
@@ -1,25 +1,47 @@
 import { Inter, Lobster } from "next/font/google";
 import styles from "./ContactForm.module.css";
+import { useRef } from "react";
 
 const font0 = Lobster({ subsets: ["latin"], weight: "400" });
 const font1 = Inter({ subsets: ["latin"] });
 
 export const ContactForm = () => {
+  const nameInputRef = useRef(null);
+  const emailInputRef = useRef(null);
+  const messageInputRef = useRef(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const name = nameInputRef.current.value;
+    const email = emailInputRef.current.value;
+    const message = messageInputRef.current.value;
+    console.log({ name, email, message });
+  };
+
   return (
-    <div className={styles.formContainer+" "+font1.className}>
-      <h1 className={font0.className}>Have something on your mind? Tell me!</h1>
-      <form>
+    <div className={styles.formContainer + " " + font1.className}>
+      <h1 className={font0.className}>
+        Have something on your mind? Tell me!
+      </h1>
+      <form onSubmit={handleSubmit}>
         <label>
           <h3>Your Name</h3>
-          <input type="text" />
+          <input ref={nameInputRef} type="text" required />
         </label>
         <label>
           <h3>Your Email</h3>
-          <input type="email" />
+          <input ref={emailInputRef} type="email" required />
         </label>
         <label>
           <h3>Your Message</h3>
-          <textarea name="" id="" rows="7"></textarea>
+          <textarea
+            ref={messageInputRef}
+            name=""
+            id=""
+            rows="7"
+            required
+          ></textarea>
         </label>
         <button>Send Message</button>
       </form>
