56c37939f1a8072b879520608c24d63745f255ad
mohammadT9030 <torabi9030@gmail.com>
2023-09-04 20:15:47 +0330

    Changing error message

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
       return res.status(201).json("Message received successfully!");
     } catch (err) {
       console.log(err);
-      return res.status(500).json("The message was not sent!");
+      return res.status(500).json("Message was not sent!");
     }
   }
 }
