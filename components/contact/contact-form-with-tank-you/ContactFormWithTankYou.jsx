7ea116e56bd27b69b1365a1152199d3ad72858ef
mohammadT9030 <torabi9030@gmail.com>
2023-09-05 19:40:55 +0330

    Moving ToastContainer to ContactPage

--- components/contact/contact-form-with-tank-you/ContactFormWithTankYou.jsx
+++ components/contact/contact-form-with-tank-you/ContactFormWithTankYou.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { ContactForm } from "../contact-form/ContactForm";
 import { ThankYou } from "../tank-you/ThankYou";
 import { sendMessageHandler } from "./sendMessageHandler";
-import { ToastContainer } from "react-toastify";
 
 export const ContactFormWithTankYou = () => {
   const [showTankYou, setShowTankYou] = useState(false);
--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,12 +1,10 @@
 import { Navigation } from "./navigation/Navigation";
-import { ToastContainer } from "react-toastify";
 
 export const Layout = (props) => {
   return (
     <>
       <Navigation />
       {props.children}
-      <ToastContainer theme="dark" position="bottom-right" />
     </>
   );
 };
