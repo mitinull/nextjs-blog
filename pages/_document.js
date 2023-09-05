4a3ff9f515a153e25bf591ef19f649fed8eddafa
mohammadT9030 <torabi9030@gmail.com>
2023-09-05 19:31:21 +0330

    Removing extra lang = "en"

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head lang="en"/>
+    <Html>
+      <Head lang="en" />
       <body>
         <Main />
         <NextScript />
